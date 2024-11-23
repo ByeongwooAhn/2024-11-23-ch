088ddfdc9fb0be82865323ea94e8e915f61361b6
ByeongwooAhn <abyungwoo@gmail.com>
2024-11-23 14:24:32 +0900

    Creat ApiV1ChatRoomController

---
+++ src/main/java/com/ll/ch/damain/chat/chatRoom/controller/ApiV1ChatControllers.java
@@ -0,0 +1,14 @@
+package com.ll.ch.damain.chat.chatRoom.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/chat/rooms")
+public class ApiV1ChatControllers {
+    @GetMapping()
+    public String getChatRooms() {
+        return "채팅방 목록!";
+    }
+}
